e0c8645eaadb3559d9d72da0a54a8c08ab791275
kishorchouhan <devkishorbwr@gmail.com>
2019-12-21 23:56:04 +0530

    changes in .some

---
+++ Array/some.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*****************NATIVE SOME*************************/
+
+Array.prototype.mySome = function(callback, thisArg) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback.call(thisArg, this[i], i, this)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Test#1
+var passed = [12, 5, 8, 130, 44].mySome(function(element) {
+  return element >= 200;
+});
+console.log("some: " + passed); //some: false
+
+// Test#2
+var passed = [12, 5, 8, 130, 44].mySome(function(element) {
+  return element >= 100;
+});
+console.log("some: " + passed); //some: true
+
+// Test#3
+var passed = [].mySome(function(element) {
+  return element >= 100;
+});
+console.log("some: " + passed); //some: false
