ca52ea1f856e3810d6f8d1258d3058e3193f0181
kishorchouhan <devkishorbwr@gmail.com>
2020-01-03 23:43:23 +0530

    name error fix

--- Array/push.js
+++ Array/push.js
@@ -2,7 +2,7 @@
 
 /*****************NATIVE PUSH*************************/
 
-Array.prototype.mypush = function(...arg) {
+Array.prototype.myPush = function(...arg) {
   let newArr = [...this, ...arg];
   for (let i = 0; i < newArr.length; i++) {
     this[i] = newArr[i];
@@ -12,5 +12,5 @@ Array.prototype.mypush = function(...arg) {
 
 // Test#1
 let arr = [1, 2, 3];
-console.log(arr.mypush(4, 5, 6));
+console.log(arr.myPush(4, 5, 6));
 console.log(arr);
