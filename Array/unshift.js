46f0357b580028b9b02c28e3513f715a919089c6
kishorchouhan <devkishorbwr@gmail.com>
2020-03-05 00:43:50 +0530

    native unshift corrected

--- Array/unshift.js
+++ Array/unshift.js
@@ -5,28 +5,10 @@
 
 // Note: This method changes the length of an array.
 
-Array.prototype.myUnshift = function(elements) {
-  var argCount = arguments.length;
-  var i = this.length - 1;
-  // var len = array.length;
-  this.length = this.length + argCount;
-
-  if (argCount > 0) {
-    if (this.length + argCount > 9.0071993e15) {
-      throw new TypeError('Too many arguments...');
-    }
+Array.prototype.myUnshift = function(...arg) {
+  let newArr = [...arg, ...this];
+  for (let i = 0; i < newArr.length; i++) {
+    this[i] = newArr[i];
   }
-
-  while (i >= argCount - 1) {
-    this[i] = this[i - argCount];
-    i--;
-  }
-
-  var l = 0;
-  while (l < arguments.length) {
-    this[l] = arguments[l];
-    l++;
-  }
-
-  return this.length;
+  return newArr.length;
 };
