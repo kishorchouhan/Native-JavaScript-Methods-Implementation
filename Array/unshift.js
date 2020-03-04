68830a964242b2bf961eccf4f6e77c9909931240
kishorchouhan <devkishorbwr@gmail.com>
2020-03-05 00:44:25 +0530

    test case added in unshift

--- Array/unshift.js
+++ Array/unshift.js
@@ -12,3 +12,26 @@ Array.prototype.myUnshift = function(...arg) {
   }
   return newArr.length;
 };
+
+//Test#1
+
+var arr = [1, 2, 3];
+var op1 = arr.myUnshift(9);
+console.log('O/P#1: ', { op1, arr });
+
+// Test#2
+
+var cars = ['BMW'];
+var op2 = cars.myUnshift('Oody');
+console.log('O/P#2: ', { op2, cars });
+
+// Test#3
+
+var operatingSystems = ['Windows 98', 'Windows XP', 'OSX 10.1'];
+var op3 = operatingSystems.myUnshift('Linux Mint', 'RedHat', 'Slackware');
+console.log('O/P#3: ', { op3, operatingSystems });
+
+//Test#4
+var num = [1, 2, 3];
+var op4 = num.myUnshift({ gh: 4, hvg: 5 }, [3, 2]);
+console.log('O/P#4: ', { op4, num });
