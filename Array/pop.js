84c33446ed322ddd26bbbd0eb8105dc73858b7a0
kishorchouhan <devkishorbwr@gmail.com>
2020-03-06 23:19:16 +0530

    test cases added

--- Array/pop.js
+++ Array/pop.js
@@ -14,3 +14,19 @@ Array.prototype.myPop = function() {
   this.length--;
   return returnValue;
 };
+
+// Test
+var arr1 = [1, 3];
+var arr2 = [1];
+var arr3 = [];
+var operatingSystems = ['Windows 98', 'Windows XP', 'OSX 10.1'];
+
+var op1 = arr1.myPop();
+var op2 = arr2.myPop();
+var op3 = arr3.myPop();
+var op4 = operatingSystems.myPop();
+
+console.log(op1, arr1);
+console.log(op2, arr2);
+console.log(op3, arr3);
+console.log(op4, operatingSystems);
